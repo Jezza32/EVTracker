02b023220ffccffba163821cf4e6bef4f0a14b8a
Peter Gerrard <peter.gerrard@red-gate.com>
2015-12-18 16:11:42 +0000

    Create all the stat item buttons.

--- EVTracker.Wpf/MainWindow.xaml.cs
+++ EVTracker.Wpf/MainWindow.xaml.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Windows;
@@ -28,90 +27,38 @@ namespace EVTracker.Wpf
     {
         public Context(IList<Pokemon> pokemonList)
         {
-            HondewBerryCommand = new RelayCommand(o => (o as Pokemon)?.ApplyStatBerry(Stat.SpecialAttack));
+            HpUpCommand = new RelayCommand(o => CurrentPokemon.ApplyStatBoost(Stat.HP));
+            PomegBerryCommand = new RelayCommand(o => CurrentPokemon.ApplyStatBerry(Stat.HP));
+            ProteinCommand = new RelayCommand(o => CurrentPokemon.ApplyStatBoost(Stat.Attack));
+            KelpsyBerryCommand = new RelayCommand(o => CurrentPokemon.ApplyStatBerry(Stat.Attack));
+            IronCommand = new RelayCommand(o => CurrentPokemon.ApplyStatBoost(Stat.Defence));
+            QualotBerryCommand = new RelayCommand(o => CurrentPokemon.ApplyStatBerry(Stat.Defence));
+            CalciumCommand = new RelayCommand(o => CurrentPokemon.ApplyStatBoost(Stat.SpecialAttack));
+            HondewBerryCommand = new RelayCommand(o => CurrentPokemon.ApplyStatBerry(Stat.SpecialAttack));
+            ZincCommand = new RelayCommand(o => CurrentPokemon.ApplyStatBoost(Stat.SpecialDefence));
+            GrepaBerryCommand = new RelayCommand(o => CurrentPokemon.ApplyStatBerry(Stat.SpecialDefence));
+            CarbosCommand = new RelayCommand(o => CurrentPokemon.ApplyStatBoost(Stat.Speed));
+            TamatoBerryCommand = new RelayCommand(o => CurrentPokemon.ApplyStatBerry(Stat.Speed));
 
             PokemonList = pokemonList;
             CurrentPokemon = pokemonList.FirstOrDefault();
         }
 
+        public ICommand HpUpCommand { get; }
+        public ICommand ProteinCommand { get; set; }
+        public ICommand IronCommand { get; set; }
+        public ICommand CalciumCommand { get; set; }
+        public ICommand ZincCommand { get; set; }
+        public ICommand CarbosCommand { get; set; }
+
+        public ICommand PomegBerryCommand { get; set; }
+        public ICommand KelpsyBerryCommand { get; set; }
+        public ICommand QualotBerryCommand { get; set; }
+        public ICommand HondewBerryCommand { get; }
+        public ICommand GrepaBerryCommand { get; set; }
+        public ICommand TamatoBerryCommand { get; set; }
+
         public IList<Pokemon> PokemonList { get; }
         public Pokemon CurrentPokemon { get; set; }
-        public ICommand HondewBerryCommand { get; }
-    }
-
-    public class RelayCommand : ICommand
-    {
-        private Action<object> execute;
-
-        private Predicate<object> canExecute;
-
-        private event EventHandler CanExecuteChangedInternal;
-
-        public RelayCommand(Action<object> execute)
-            : this(execute, DefaultCanExecute)
-        {
-        }
-
-        public RelayCommand(Action<object> execute, Predicate<object> canExecute)
-        {
-            if (execute == null)
-            {
-                throw new ArgumentNullException(nameof(execute));
-            }
-
-            if (canExecute == null)
-            {
-                throw new ArgumentNullException(nameof(canExecute));
-            }
-
-            this.execute = execute;
-            this.canExecute = canExecute;
-        }
-
-        public event EventHandler CanExecuteChanged
-        {
-            add
-            {
-                CommandManager.RequerySuggested += value;
-                this.CanExecuteChangedInternal += value;
-            }
-
-            remove
-            {
-                CommandManager.RequerySuggested -= value;
-                this.CanExecuteChangedInternal -= value;
-            }
-        }
-
-        public bool CanExecute(object parameter)
-        {
-            return this.canExecute != null && this.canExecute(parameter);
-        }
-
-        public void Execute(object parameter)
-        {
-            this.execute(parameter);
-        }
-
-        public void OnCanExecuteChanged()
-        {
-            EventHandler handler = this.CanExecuteChangedInternal;
-            if (handler != null)
-            {
-                //DispatcherHelper.BeginInvokeOnUIThread(() => handler.Invoke(this, EventArgs.Empty));
-                handler.Invoke(this, EventArgs.Empty);
-            }
-        }
-
-        public void Destroy()
-        {
-            this.canExecute = _ => false;
-            this.execute = _ => { return; };
-        }
-
-        private static bool DefaultCanExecute(object parameter)
-        {
-            return true;
-        }
     }
 }
