1b05c6eefdcaaec1b6534f18f4e739d2007288ad
Peter Gerrard <peter.gerrard@red-gate.com>
2016-02-12 14:26:25 +0000

    Add combo boxes for natures and species

--- Pokemon.EVTracker.Html/index.js
+++ Pokemon.EVTracker.Html/index.js
@@ -1,5 +1,29 @@
 ﻿var $ = require('jquery');
+var ko = require('knockout');
 
-$.getJSON("http://localhost:25072/api/v0/Natures", function(data) {
-    console.log(data);
+function AppViewModel() {
+    this.allSpecies = ko.observableArray();
+    this.species = ko.observable();
+    this.allNatures = ko.observableArray();
+    this.nature = ko.observable();
+}
+
+function normaliseDexNumber(dexNum) {
+    var s = String(dexNum);
+    while (s.length < 3) {
+        s = '0' + s;
+    }
+    return s;
+}
+
+var appViewModel = new AppViewModel();
+
+ko.applyBindings(appViewModel);
+
+$.getJSON("http://localhost:25072/api/v0/Natures", function (data) {
+    appViewModel.allNatures(data);
+});
+
+$.getJSON("http://localhost:20640/api/v0/Species", function (data) {
+    appViewModel.allSpecies(data);
 });
